a0c34891c0c95f2a3a0c52ddeac45bd4977f0a33
taarun96 <taarun96@gmail.com>
2020-11-04 23:50:37 +0530

    Folder Name Changes

---
+++ BootCamp-WeeklyTasks/Task-Week1/JS-Objects-Problems/0B.js
@@ -0,0 +1,10 @@
+// Problem 0 : Part B (15 mins):
+// Iterating with JSON object’s Values
+// Above is some information about my car. As you can see, I am not the best driver.
+// I have caused a few accidents.
+// Please update this driving record so that I can feel better about my driving skills.
+
+// 1. Loop over the accidents array. Change atFaultForAccident from true to false.
+// 2. Print the dated of my accidents
+
+
